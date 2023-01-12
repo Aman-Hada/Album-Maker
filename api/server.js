7f71f5f9fd87959b55b0760c959b607c181151ae
Aman-Hada <amansinghhada.201cs206@nitk.edu.in>
2023-01-12 16:10:53 +0530

    setting node api endpoint to cloudinary

--- api/server.js
+++ api/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const cors = require('cors');
 const {json} = require('body-parser');
-//const axios = require('axios');
+const axios = require('axios');
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 
+const {parsed: config }=dotenv.config();
+const auth={
+    username: config.API_KEY,
+    password: config.API_SECRET,
+}
+const BASE_URL=`https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`
+
 app.get('/photos', async(req,res)=>{
-    return res.send({mesge : 'hello'});
+    const response= await axios.get(BASE_URL,{
+        auth,
+    });
+    return res.send(response.data);
 });
 
 const PORT=6000;
