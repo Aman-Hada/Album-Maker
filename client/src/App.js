0ede04276e36a5c7dd20cbf71d53dc58dee60a9b
Aman-Hada <amansinghhada.201cs206@nitk.edu.in>
2023-01-14 13:40:06 +0530

    connected the endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import React, {useState} from 'react'
-import images from './app-api.json'
+import React, {useState, useEffect} from 'react'
+import { getImages } from './api';
 import './App.css'
 const App = () => {
-  const [imageList, setImageList] = useState(images.resources)
+  const [imageList, setImageList] = useState([])
+  useEffect(()=>{
+    const fetchdata = async()=>{
+      const responseJson=await getImages();
+      setImageList(responseJson.resources);
+    }
+    fetchdata();
+  }, []);
+  
   return (
     <div className='image-grid'>{
       imageList.map((image)=>(<img src={image.url} alt={image.public_id}></img>))
---
+++ client/src/api.js
@@ -0,0 +1,7 @@
+const API_URL=process.env.REACT_APP_API_URL;
+
+export const getImages = async()=>{
+    const response = await fetch(`${API_URL}/photos`);
+    const responseJson= await response.json();
+    return responseJson;
+};
