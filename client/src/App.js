b6dae9c95f862b3b2f5ff965ec2cc82aa3f1c4e0
Aman-Hada <amansinghhada.201cs206@nitk.edu.in>
2023-01-19 16:20:43 +0530

    some adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ const App = () => {
   const [imageList, setImageList] = useState([]);
   const [nextCursor, setNextCursor] = useState(null);
   const [searchValue, setSearchValue]= useState('');
+  const [temp, setTempValue]=useState(0);
   useEffect(()=>{
     
     const fetchdata = async()=>{
-      const responseJson=await getImages();
+      const responseJson=await getImages();   
       setImageList(responseJson.resources);
       setNextCursor(responseJson.next_cursor);
     }
@@ -23,23 +24,36 @@ const App = () => {
   const loadSearchHandler=async(event)=>{
     event.preventDefault();
     const responseJson=await getSearch(searchValue,nextCursor);
+    setTempValue(1);
+    setImageList(responseJson.resources);
+    setNextCursor(responseJson.next_cursor);
+    
+    
+  }
+  const imageRestoreHandler=async()=>{
+    setSearchValue('');
+    const responseJson= await getImages();
+    setTempValue(0);
     setImageList(responseJson.resources);
     setNextCursor(responseJson.next_cursor);
     
   }
-  
   return (<>
     <form className='form' onSubmit={loadSearchHandler}>
       <input value={searchValue} onChange={(event)=> setSearchValue(event.target.value)} required='required' placeholder='enter a search value...' ></input>
       <button type='submit'>search</button>
       <button type='reset'>clear</button>
+      {temp===1?<button type='button' onClick={imageRestoreHandler}>All Images</button>:''}
     </form>
-    
     <div className='image-grid'>{
       imageList.map((image)=>(<img src={image.url} alt={image.public_id} key={image.public_id}></img>))
     }</div>
     <div className='footer'>
       {nextCursor && <button onClick={loadImagesHandler} >Load More Images</button>}
+    </div>
+    <div className='hmmm'>
+      { imageList.length===0 ? <p>Sorry, no image found of that name</p>: ''}
+
     </div>
     </>
   );
